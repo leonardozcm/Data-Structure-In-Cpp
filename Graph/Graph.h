c6073c88a777b1c736350c7e4b4e7f5145ad7da8
leoanrdozcm <597714572@qq.com>
2018-03-10 13:01:22 +0800

    Graph.h built basically

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -12,6 +12,7 @@
 
 using namespace std;
 #define MaxVertex 5
+#define UnUsed -1
 typedef int Vertex;
 
 
@@ -20,8 +21,8 @@ class Graph {
 public:
     class GraphNode {
     public:
-        Vertex v{};
-        double weight{};
+        Vertex v;
+        double weight;
 
         GraphNode() = default;
 
@@ -38,40 +39,60 @@ public:
 
     Graph();
 
-    void Add(Vertex ver, list<pair<Vertex, double >> &link_list, T t);
-
-    void PutIntoStack();
-
     void TopSort();
 
-    void Link(Vertex startVertex, Vertex pointVertex, double weight);
+    void Add(Vertex ver, list<pair<Vertex, double >> &link_list, T t);
 
-    void Link(Vertex startVertex, Vertex pointVertex);
+    Graph<T> &Link(Vertex startVertex, Vertex pointVertex, double weight);
 
-    void DoubleLink(Vertex startVertex, Vertex, double weight);
+    Graph<T> &Link(Vertex startVertex, Vertex pointVertex) {
+        return Link(startVertex, pointVertex, 1);
+    };
 
-    void DoubleLink(Vertex startVertex, Vertex pointVertex);
+    Graph<T> &DoubleLink(Vertex startVertex, Vertex pointVertex, double weight);
+
+    Graph<T> &DoubleLink(Vertex startVertex, Vertex pointVertex) {
+        return DoubleLink(startVertex, pointVertex, 1);
+    };
 
 private:
+    int unusedVex = 0;
+
     list<GraphNode> vertex_vector[MaxVertex];
     stack<Vertex> InDegreeZero;
     map<Vertex, T> keyMap;
     int InDegree[MaxVertex]{};
 
+    void PutIntoStack();
+
+    void SetUsed(Vertex vertex) {
+        if (InDegree[vertex] == UnUsed) {
+            InDegree[vertex] = 0;
+        }
+    }
 };
 
 template<class T>
 Graph<T>::Graph() {
     for (auto &i : InDegree) {
-        i = 0;
+        i = UnUsed;
     }
 }
 
+/**
+ *
+ * @tparam T 储存在node里面的数据类型
+ * @param ver 起始的顶点
+ * @param link_list ：Vertex 指向的顶点，weight为权重,link_list为这样一个链表
+ * @param t 储存在node里面的数据
+ */
 template<class T>
 void Graph<T>::Add(Vertex ver, list<pair<Vertex, double >> &link_list, T t) {
     keyMap.insert(pair<Vertex, T>(ver, t));
     list<GraphNode> add_list;
+    SetUsed(ver);
     for (auto itor : link_list) {
+        SetUsed(itor.first);
         InDegree[itor.first]++;
         GraphNode graphNode(itor.first, itor.second);
         add_list.push_back(graphNode);
@@ -79,15 +100,15 @@ void Graph<T>::Add(Vertex ver, list<pair<Vertex, double >> &link_list, T t) {
 
 
     vertex_vector[ver] = add_list;
-    auto itor = link_list.begin();
 }
 
 template<class T>
 void Graph<T>::PutIntoStack() {
-    for (auto &i:InDegree) {
-        if (!i) {
+    for (int i = 0; i < MaxVertex; i++) {
+        if (!InDegree[i]) {
             InDegreeZero.push(i);
         }
+        if(InDegree[i]==UnUsed){ unusedVex++;}
     }
 }
 
@@ -96,19 +117,58 @@ void Graph<T>::TopSort() {
     Vertex v;
     PutIntoStack();
     int counter = 0;
+
     while (!InDegreeZero.empty()) {
         counter++;
         v = InDegreeZero.top();
         InDegreeZero.pop();
-        printf("%d -> ", v);
+        if (counter == MaxVertex-unusedVex) {
+            printf("%d \nThe way ends.", v);
+        } else { printf("%d -> ", v); }
+
         for (auto itor:vertex_vector[v]) {
             if (--InDegree[itor.v] == 0)
                 InDegreeZero.push(itor.v);
         }
     }
-    if (counter != MaxVertex) {
+
+    if (counter != MaxVertex-unusedVex) {
         printf("Graph has a circle.\n");
     }
+}
+
+/**
+ * 以添加指向性向量的形式链接两顶点
+ * @tparam T 测试阶段没写
+ * @param startVertex 起始点
+ * @param pointVertex 指向点
+ * @param weight 权重
+ * @return 链式操作
+ */
+template<class T>
+Graph<T> &Graph<T>::Link(Vertex startVertex, Vertex pointVertex, double weight) {
+    GraphNode graphNode(pointVertex, weight);
+    vertex_vector[startVertex].push_back(graphNode);
+    SetUsed(startVertex);
+    SetUsed(pointVertex);
+    InDegree[pointVertex]++;
+    return *this;
+}
+
+/*
+ * 同上
+ */
+template<class T>
+Graph<T> &Graph<T>::DoubleLink(Vertex startVertex, Vertex pointVertex, double weight) {
+    SetUsed(startVertex);
+    SetUsed(pointVertex);
+    GraphNode graphNode(pointVertex, weight);
+    vertex_vector[startVertex].push_back(graphNode);
+    GraphNode graphNode1(startVertex, weight);
+    vertex_vector[pointVertex].push_back(graphNode1);
+    InDegree[pointVertex]++;
+    InDegree[startVertex]++;
+    return *this;
 }
 
 #endif //DATASTRUCTURESINCPP_GRAPH_H
